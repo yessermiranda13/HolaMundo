a6dc490082b5032c196eb0a6b5fc13b495fa4417
crisjavi55 <40309744+crisjavi55@users.noreply.github.com>
2018-06-16 17:46:54 -0700

    Cristian
    ya hijo de tu mama, solo la modificación del código en github agregar dos console.writeline

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace HolaMundo
         {
             Console.WriteLine("Hola Mundo!");
             Console.WriteLine("Aquí estuvo Yesser .l.");
+            Console.WriteLine("Hola Mundo!!!");
+            Console.WriteLine("Aquí estuvo Cristian perras viejas");
+           
         }
     }
 }
